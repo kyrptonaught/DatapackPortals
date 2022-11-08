a4d37df54758d980793836f71e8ea2a50534c90e
kyrptonaught <anthonybuttillo10@gmail.com>
2022-11-08 13:39:44 -0500

    Add support for flat portals

--- src/main/java/net/kyrptonaught/datapackportals/portalTypes/DefaultPortal.java
+++ src/main/java/net/kyrptonaught/datapackportals/portalTypes/DefaultPortal.java
@@ -1,9 +1,7 @@
 package net.kyrptonaught.datapackportals.portalTypes;
 
-import net.kyrptonaught.customportalapi.portal.PortalIgnitionSource;
-import net.kyrptonaught.customportalapi.util.ColorUtil;
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
 import net.kyrptonaught.customportalapi.util.PortalLink;
-import net.kyrptonaught.datapackportals.DatapackPortalsMod;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -13,24 +11,28 @@ public class DefaultPortal implements PortalData {
     public String ignitionSource;
     public String dim;
     public String returnDim;
+    public String portalType;
     public int r, g, b;
 
     @Override
     public PortalLink toLink(Identifier identifier) {
-        if (block == null) DatapackPortalsMod.logerror(identifier + " missing field: block");
-        if (ignitionType == null) DatapackPortalsMod.logerror(identifier + " missing field: ignitionType");
-        if (ignitionSource == null) DatapackPortalsMod.logerror(identifier + " missing field: ignitionSource");
-        if (dim == null) DatapackPortalsMod.logerror(identifier + " missing field: dim");
+        PortalLink link = new PortalLink();
+        CustomPortalBuilder builder = CustomPortalBuilder.beginPortal(link)
+                .frameBlock(new Identifier(block))
+                .destDimID(new Identifier(dim))
+                .tintColor(r, g, b);
 
-        PortalLink link = new PortalLink(new Identifier(block), new Identifier(dim), ColorUtil.getColorFromRGB(r, g, b));
-        if (ignitionType.equalsIgnoreCase("block"))
-            link.portalIgnitionSource = PortalIgnitionSource.FIRE;
-        else if (ignitionType.equalsIgnoreCase("fluid"))
-            link.portalIgnitionSource = PortalIgnitionSource.FluidSource(Registry.FLUID.get(new Identifier(ignitionSource)));
+        if (ignitionType.equalsIgnoreCase("fluid"))
+            builder.lightWithFluid(Registry.FLUID.get(new Identifier(ignitionSource)));
         else if (ignitionType.equalsIgnoreCase("item"))
-            link.portalIgnitionSource = PortalIgnitionSource.ItemUseSource(Registry.ITEM.get(new Identifier(ignitionSource)));
+            builder.lightWithItem(Registry.ITEM.get(new Identifier(ignitionSource)));
 
-        if (returnDim != null) link.returnDimID = new Identifier(returnDim);
+        if (returnDim != null)
+            builder.returnDim(new Identifier(returnDim), false);
+
+        if (portalType != null && portalType.equalsIgnoreCase("flat"))
+            builder.flatPortal();
+
         return link;
     }
 }
